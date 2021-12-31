118a538404c369ba47f31e71313de4c943b0aa51
syler11 <nemeth.szilard82@gmail.com>
2021-12-31 10:49:21 +0000

    USer date in put working

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import datetime
 
+date_entry = input('Enter a date in YYYY-MM-DD format: ')
+early_sunset = ""
+early_sunrise = ""
+late_sunrise = ""
+late_sunset = ""
    
-def get_date():
+def get_date(date_entry):
     print("Please enter the date you would like to calculate sunrise and sunset for")
     print("Example: 2020, 10, 20\n")
 
-    date1 = datetime.datetime(2021, 10, 21)
+    day, month, year = map(int, date_entry.split('-'))
+    converted_date = datetime.date(year, month, day)
  
-    day_of_year = int(date1.strftime('%j'))
-
-    print(day_of_year)
+    day_of_year = int(converted_date.strftime('%j'))
  
     if day_of_year >= 172 and day_of_year <355:
         x =  day_of_year -172
@@ -19,7 +23,10 @@ def get_date():
         print(x)
     elif day_of_year >= 355:
         x = 355 - day_of_year
-        # print(x)
-        print(type(x))
+        print(x)
 
-get_date()
+def daily_time_difference
+    
+
+
+get_date(date_entry)
