8e654143492d4d0a011814e14affa57b38b26207
syler11 <nemeth.szilard82@gmail.com>
2021-12-31 10:01:44 +0000

    test

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-from datetime import date
- 
+import datetime
 
+   
 def get_date():
-    """
-    User input date to calculate and return reuquired sunrise and sunset data
-    """
     print("Please enter the date you would like to calculate sunrise and sunset for")
     print("Example: 2020, 10, 20\n")
 
-    date_str = input("Enter your date here: ")
-
-    converted_date = date_str
-
-    print(converted_date)
-
-
-def calculate_day(user_data):
+    date1 = datetime.datetime(2021, 10, 21)
  
-    day_of_year = user_data.strftime('%j')
+    day_of_year = int(date1.strftime('%j'))
+
+    print(day_of_year)
  
     if day_of_year >= 172 and day_of_year <355:
         x =  day_of_year -172
@@ -27,9 +19,7 @@ def calculate_day(user_data):
         print(x)
     elif day_of_year >= 355:
         x = 355 - day_of_year
-        print(x)
+        # print(x)
+        print(type(x))
 
-user_data = get_date()
-# calculate_day(user_data)
-
-print(converted_date)
+get_date()
